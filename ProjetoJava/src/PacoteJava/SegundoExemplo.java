243a405b71df4b06b6b286a350a9f015f03e4e99
Laercio-pc <lndsilva@hotmail.com>
2020-08-18 21:48:21 -0300

    iniciando projeto java com o segundo H.

---
+++ ProjetoJava/src/PacoteJava/SegundoExemplo.java
@@ -0,0 +1,16 @@
+package PacoteJava;
+
+public class SegundoExemplo {
+    
+    /*Está classe não tem o método main que é o
+    método principal.
+    */
+    
+    public static void escrever(){
+        int x = 10;
+        String nome = "Valor = ";
+        
+        System.out.println(nome + " "+x);
+    }
+    
+}
