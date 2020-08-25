285eb16e0b55106f076f871b3d4d49f79d87ed33
Laercio-pc <lndsilva@hotmail.com>
2020-08-25 20:10:19 -0300

    metodos com e sem parametros

--- ProjetoJava/src/PacoteJava/PrimeiroMetodo.java
+++ ProjetoJava/src/PacoteJava/PrimeiroMetodo.java
@@ -1,9 +1,25 @@
 package PacoteJava;
 
 public class PrimeiroMetodo {
+    
     //criando um método com parâmetros e com retorno
-    public static double somaValor(double v1,double v2){
+    public static double somaValor(double v1, double v2){
         return (v1+v2);
     }
-    
+    //método sem parâmetro e sem retorno
+    //Executa por si mesmo
+    public static void somarValor(){
+        double v1, v2, resp;
+        v1=10.5;
+        v2=10.5;
+        resp = v1+v2;
+        System.out.println("Somar Valor = "+resp);
+    }
+    //Método sem retorno mas com parâmetros
+    public static void somandoValor(double v1,double v2){
+        
+        double resp = (v1+v2);
+        
+        System.out.println("Somando valor = "+resp);
+    }
 }
---
+++ ProjetoJava/src/PacoteJava/TestaClasse.java
@@ -0,0 +1,20 @@
+package PacoteJava;
+
+public class TestaClasse {
+    
+    //método principal é uma classe executável
+    public static void main(String args[]){
+        
+        //Para executar um método de outra classe segue:
+        double resp = PrimeiroMetodo.somaValor(20.55, 10.53);
+        
+        System.out.println("A soma = "+resp);
+        
+        PrimeiroMetodo.somarValor();
+        
+        PrimeiroMetodo.somandoValor(31.50, 21.50);
+               
+        
+    }
+    
+}
