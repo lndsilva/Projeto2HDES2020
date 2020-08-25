5173e8a3fe4362277209ffef0bbedc82b07e89c6
Laercio-pc <lndsilva@hotmail.com>
2020-08-25 19:29:26 -0300

    metodos

---
+++ ProjetoJava/src/PacoteJava/PrimeiroMetodo.java
@@ -0,0 +1,9 @@
+package PacoteJava;
+
+public class PrimeiroMetodo {
+    //criando um método com parâmetros e com retorno
+    public static double somaValor(double v1,double v2){
+        return (v1+v2);
+    }
+    
+}
